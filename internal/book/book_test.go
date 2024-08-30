7ae849f7e9e8c75f2fe27052fb30d7406a76358a
Ahmed Alsaedi <aalsaedi265@gmail.com>
2024-08-30 11:48:35 -0400

    test sucessful

--- internal/book/book_test.go
+++ internal/book/book_test.go
@@ -33,3 +33,48 @@ func TestAddOrder(t *testing.T){
 		t.Errorf("expected order amount %d, got %d", order.Amount, addedOrder.Amount)
 	}
 }
+
+func TestListOrders(t *testing.T){
+	orderBook := NewBook()
+
+	order1 := Order{
+		ID:     "1",
+		Type:   Buy,
+		Price:  100.0,
+		Amount: 5,
+	}
+	order2 := Order{
+		ID:     "2",
+		Type:   Sell,
+		Price:  105.0,
+		Amount: 3,
+	}
+
+	initialBuyLength := len(orderBook.ListOrders(Buy))
+	orderBook.AddOrder(order1)
+	newBuyLength := len(orderBook.ListOrders(Buy))
+	if newBuyLength != initialBuyLength+1{
+		t.Fatalf("expected buy orders length to increase by 1, but it didn't (initial: %d, new: %d)", initialBuyLength, newBuyLength)
+	}
+
+	initialSellLength := len(orderBook.ListOrders(Sell))
+	orderBook.AddOrder(order2)
+	newSellLength := len(orderBook.ListOrders(Sell))
+	if newSellLength != initialSellLength+1{
+		t.Fatalf("expected sell orders length to increase by 1, but it didn't (initial: %d, new: %d)", initialSellLength, newSellLength)
+	}
+
+	// Test Buy Orders - check the last order added
+	buyOrders := orderBook.ListOrders(Buy)
+	lastBuyOrder := buyOrders[len(buyOrders)-1]
+	if lastBuyOrder.ID != order1.ID {
+		t.Errorf("expected buy order ID '%s', got '%s'", order1.ID, lastBuyOrder.ID)
+	}
+
+	// Test Sell Orders - check the last order added
+	sellOrders := orderBook.ListOrders(Sell)
+	lastSellOrder := sellOrders[len(sellOrders)-1]
+	if lastSellOrder.ID != order2.ID {
+		t.Errorf("expected sell order ID '%s', got '%s'", order2.ID, lastSellOrder.ID)
+	}
+}
